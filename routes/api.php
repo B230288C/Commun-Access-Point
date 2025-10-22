61ecba29a8edc56d04f5b9d4321c9e33bf19f01d
B230288C <B230288C@sc.edu.my>
2025-10-22 09:12:46 +0800

    done api for appointment

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,18 @@
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+<?php
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppointmentController;
+
+Route::prefix('appointments')->group(function () {
+    // 创建预约
+    Route::post('/', [AppointmentController::class, 'store']);
+
+    // 查看单个预约
+    Route::get('/{id}', [AppointmentController::class, 'show']);
+
+    // 更新预约
+    Route::put('/{id}', [AppointmentController::class, 'update']);
+
+    // 取消预约
+    Route::patch('/{id}/cancel', [AppointmentController::class, 'cancel']);
 });
